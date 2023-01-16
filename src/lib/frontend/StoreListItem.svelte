6424320a82a948a6094183470aecf660303d9649
Mateusz <matiutd888@gmail.com>
2023-01-16 20:31:33 +0100

    Add pictures to list items

--- src/lib/frontend/StoreListItem.svelte
+++ src/lib/frontend/StoreListItem.svelte
@@ -3,9 +3,23 @@
   export let value: Shop;
 </script>
 
-<div>
-  <!-- <img src={value.photoUrl} /> -->
-  <h2>{value.name}</h2>
+<div style="display: flex;" class="container">
+  <img src={value.photoUrl} height="50p" alt="Picture of {value.name}" />
+  <script
+    src="https://kit.fontawesome.com/a076d05399.js"
+    crossorigin="anonymous"
+  ></script>
+  <div style="align-items: stretch; justify-content: center; height: 50px">
+    <a href={value.googleMapsLink}>
+      <i
+        style="font-size:24px; text-align: center;"
+        class="fas fa-location-arrow"
+      />
+    </a>
+  </div>
+  <div style="align-items: stretch; flex-grow: 1">
+    <p style="text-align: center;">{value.name}</p>
+  </div>
 </div>
 
 <style>
